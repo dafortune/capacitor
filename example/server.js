95d65d11c1552a211d56f9c3ad2c5e7d13c67d64
Damian Fortuna <damian.andres.fortuna@gmail.com>
2020-07-14 04:07:45 -0300

    Added health check protection

--- example/server.js
+++ example/server.js
@@ -5,8 +5,10 @@ const capacitor = require('../lib');
 
 const c = capacitor(app, {
   // requestRateThreshold: 5,
-  cpuThreshold: 80,
-  minCapacity: 10
+  cpuThreshold: 10,
+  minCapacity: 10,
+
+  healthCheck: '/test'
 });
 
 c.events.on('load-shedding-triggered', (a, r) => console.log('load shedding', a/r, `${a}/${r}`));
@@ -19,6 +21,10 @@ app.get('/', (req, res) => {
   for (let i = 0; i < 100000000; i++) {}
 
   res.send('Done!');
+});
+
+app.get('/test', (req, res) => {
+  res.send('OK!');
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
